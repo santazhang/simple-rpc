e924516e7be20f72cbc90bc7bf53ff711bc7f1b7
Santa Zhang <santa1987@gmail.com>
2013-05-24 17:13:23 -0400

    added the NEW_RUNNABLE macros

--- rpc/utils.h
+++ rpc/utils.h
@@ -77,6 +77,61 @@ public:
     virtual void run() = 0;
 };
 
+// helper macros for quickly generating temporary runnable objects
+#define NEW_RUNNABLE_COMBINE1(x, y) x##y
+#define NEW_RUNNABLE_COMBINE(x, y) NEW_RUNNABLE_COMBINE1(x, y)
+
+#define NEW_RUNNABLE1(type1, arg1, run_func_body) \
+    new( \
+        class NEW_RUNNABLE_COMBINE(__R__, __LINE__): public Runnable { \
+        public: \
+            NEW_RUNNABLE_COMBINE(__R__, __LINE__)(type1 arg1): arg1(arg1) {} \
+            void run() { run_func_body; } \
+        private: \
+            type1 arg1; \
+        } \
+    )(arg1)
+
+#define NEW_RUNNABLE2(type1, arg1, type2, arg2, run_func_body) \
+    new( \
+        class NEW_RUNNABLE_COMBINE(__R__, __LINE__): public Runnable { \
+        public: \
+            NEW_RUNNABLE_COMBINE(__R__, __LINE__)(type1 arg1, type2 arg2): arg1(arg1), arg2(arg2) {} \
+            void run() { run_func_body; } \
+        private: \
+            type1 arg1; \
+            type2 arg2; \
+        } \
+    )(arg1, arg2)
+
+#define NEW_RUNNABLE3(type1, arg1, type2, arg2, type3, arg3, run_func_body) \
+    new( \
+        class NEW_RUNNABLE_COMBINE(__R__, __LINE__): public Runnable { \
+        public: \
+            NEW_RUNNABLE_COMBINE(__R__, __LINE__)(type1 arg1, type2 arg2, type3 arg3): arg1(arg1), arg2(arg2), arg3(arg3) {} \
+            void run() { run_func_body; } \
+        private: \
+            type1 arg1; \
+            type2 arg2; \
+            type3 arg3; \
+        } \
+    )(arg1, arg2, arg3)
+
+#define NEW_RUNNABLE4(type1, arg1, type2, arg2, type3, arg3, type4, arg4, run_func_body) \
+    new( \
+        class NEW_RUNNABLE_COMBINE(__R__, __LINE__): public Runnable { \
+        public: \
+            NEW_RUNNABLE_COMBINE(__R__, __LINE__)(type1 arg1, type2 arg2, type3 arg3, type4 arg4): arg1(arg1), arg2(arg2), arg3(arg3), arg4(arg4) {} \
+            void run() { run_func_body; } \
+        private: \
+            type1 arg1; \
+            type2 arg2; \
+            type3 arg3; \
+            type4 arg4; \
+        } \
+    )(arg1, arg2, arg3, arg4)
+
+
 /**
  * Note: All sub class of RefCounted *MUST* have protected destructor!
  * This prevents accidentally deleting the object.
