959f3cddd1ea7bd5fda2766c47fb5c8dc0a0aa46
Santa Zhang <santa1987@gmail.com>
2013-10-02 14:19:18 -0400

    move more utility into b0

--- rpc/utils.cc
+++ rpc/utils.cc
@@ -15,104 +15,6 @@ using namespace std;
 
 namespace rpc {
 
-struct start_thread_pool_args {
-    ThreadPool* thrpool;
-    int id_in_pool;
-};
-
-void* ThreadPool::start_thread_pool(void* args) {
-    start_thread_pool_args* t_args = (start_thread_pool_args *) args;
-    t_args->thrpool->run_thread(t_args->id_in_pool);
-    delete t_args;
-    pthread_exit(NULL);
-    return NULL;
-}
-
-ThreadPool::ThreadPool(int n /* =... */): n_(n), should_stop_(false) {
-    th_ = new pthread_t[n_];
-    q_ = new Queue<function<void()>*> [n_];
-
-    for (int i = 0; i < n_; i++) {
-        start_thread_pool_args* args = new start_thread_pool_args();
-        args->thrpool = this;
-        args->id_in_pool = i;
-        Pthread_create(&th_[i], NULL, ThreadPool::start_thread_pool, args);
-    }
-}
-
-ThreadPool::~ThreadPool() {
-    should_stop_ = true;
-    for (int i = 0; i < n_; i++) {
-        Pthread_join(th_[i], nullptr);
-    }
-    // TODO probably need to check if there's jobs not executed yet in queues
-    delete[] th_;
-    delete[] q_;
-}
-
-int ThreadPool::run_async(const std::function<void()>& f) {
-    if (should_stop_) {
-        return EPERM;
-    }
-    // Randomly select a thread for the job.
-    int queue_id = rand_engine_() % n_;
-    q_[queue_id].push(new function<void()>(f));
-    return 0;
-}
-
-void ThreadPool::run_thread(int id_in_pool) {
-    int64_t cycle_last_item_found = rdtsc();
-    list<function<void()>*> jobs;
-    struct timespec sleep_req;
-    sleep_req.tv_nsec = 1;
-    sleep_req.tv_sec = 0;
-
-    for (;;) {
-        if (sleep_req.tv_nsec > 1) {
-          nanosleep(&sleep_req, NULL);
-        }
-
-        int64_t cycle_now = rdtsc();
-
-        // How long should a thread wait before stealing from
-        // other queues.
-        static const int kStealCycles = 10 * 1000 * 1000;
-
-        if (cycle_now - cycle_last_item_found > kStealCycles) {
-            // start checking other queues
-            // TODO randomize each worker thread's stealing order
-            for (auto i = 0; i < n_; ++i) {
-                q_[i].pop_many(&jobs, 1);
-                if (!jobs.empty()) {
-                    break;
-                }
-            }
-        } else {
-            q_[id_in_pool].pop_many(&jobs, 1);
-        }
-
-        if (jobs.empty()) {
-            if (should_stop_) {
-                break;
-            }
-            sleep_req.tv_nsec = std::min(1000000L, sleep_req.tv_nsec * 2);
-            continue;
-        }
-
-        sleep_req.tv_nsec = 1;
-
-        while (!jobs.empty()) {
-            auto& f = jobs.front();
-            (*f)();
-            delete f;
-            jobs.pop_front();
-        }
-
-        cycle_last_item_found = rdtsc();
-    }
-}
-
-
 int set_nonblocking(int fd, bool nonblocking) {
     int ret = fcntl(fd, F_GETFL, 0);
     if (ret != -1) {
@@ -175,7 +77,6 @@ std::string get_host_name() {
   if (gethostname(buffer, 1024) != 0) {
     Log_fatal("Failed to get hostname.");
   }
-
   return std::string(buffer);
 }
 
--- rpc/utils.h
+++ rpc/utils.h
@@ -3,7 +3,6 @@
 #include <list>
 #include <map>
 #include <functional>
-#include <random>
 
 #include <sys/types.h>
 #include <sys/time.h>
@@ -27,11 +26,11 @@ using b0::ScopedLock;
 using b0::CondVar;
 using b0::Log;
 using b0::RefCounted;
-
-// choice between spinlock & mutex:
-// * when n_thread > n_core, use mutex
-// * on virtual machines, use mutex
-
+using b0::Queue;
+using b0::Counter;
+using b0::Timer;
+using b0::Rand;
+using b0::ThreadPool;
 
 // use spinlock for short critical section
 typedef SpinLock ShortLock;
@@ -39,114 +38,11 @@ typedef SpinLock ShortLock;
 // use mutex for long critical section
 typedef Mutex LongLock;
 
-
-/**
- * Thread safe queue.
- */
-template<class T>
-class Queue: public NoCopy {
-    std::list<T>* q_;
-    pthread_cond_t not_empty_;
-    pthread_mutex_t m_;
-
-public:
-
-    Queue(): q_(new std::list<T>) {
-        Pthread_mutex_init(&m_, NULL);
-        Pthread_cond_init(&not_empty_, NULL);
-    }
-
-    ~Queue() {
-        Pthread_cond_destroy(&not_empty_);
-        Pthread_mutex_destroy(&m_);
-        delete q_;
-    }
-
-    void push(const T& e) {
-        Pthread_mutex_lock(&m_);
-        q_->push_back(e);
-        Pthread_cond_signal(&not_empty_);
-        Pthread_mutex_unlock(&m_);
-    }
-
-    void pop_many(std::list<T>* out, int count) {
-        Pthread_mutex_lock(&m_);
-        if (q_->empty()) {
-          Pthread_mutex_unlock(&m_);
-          return;
-        }
-
-        while (count > 0) {
-          out->push_back(q_->front());
-          q_->pop_front();
-          --count;
-        }
-
-        Pthread_mutex_unlock(&m_);
-    }
-
-    T pop() {
-        Pthread_mutex_lock(&m_);
-        while (q_->empty()) {
-            Pthread_cond_wait(&not_empty_, &m_);
-        }
-        T e = q_->front();
-        q_->pop_front();
-        Pthread_mutex_unlock(&m_);
-        return e;
-    }
-};
-
-class Rand {
-    std::mt19937 rand_;
-public:
-    Rand() {
-        struct timeval now;
-        gettimeofday(&now, NULL);
-        rand_.seed(now.tv_sec + now.tv_usec + (long long) pthread_self() + (long long) this);
-    }
-    std::mt19937::result_type next() {
-        return rand_();
-    }
-    std::mt19937::result_type operator() () {
-        return rand_();
-    }
-};
-
-class ThreadPool: public RefCounted {
-    int n_;
-    Rand rand_engine_;
-    pthread_t* th_;
-    Queue<std::function<void()>*>* q_;
-    bool should_stop_;
-
-    static void* start_thread_pool(void*);
-    void run_thread(int id_in_pool);
-
-protected:
-    ~ThreadPool();
-
-public:
-    ThreadPool(int n = 64);
-
-    // return 0 when queuing ok, otherwise EPERM
-    int run_async(const std::function<void()>&);
-};
-
-using b0::Counter;
-using b0::Timer;
-
 int set_nonblocking(int fd, bool nonblocking);
 
 int find_open_port();
+
 std::string get_host_name();
-
-inline uint64_t rdtsc() {
-    uint32_t hi, lo;
-    __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
-    return (((uint64_t) hi) << 32) | ((uint64_t) lo);
-}
-
 
 }
 
