03fddf25658ef589d9715aa7dedb7b84e1207449
Santa Zhang <santa1987@gmail.com>
2013-10-23 13:26:22 -0400

    update gperftools-testharness.sh

--- gperftools-testharness.sh
+++ gperftools-testharness.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../gperftools/.libs
-export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:../gperftools/.libs
+export GPERFTOOLS_ROOT=../gperftools
+export GPERFTOOLS_REPORT=report.prof
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GPERFTOOLS_ROOT/.libs
+export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:$GPERFTOOLS_ROOT/.libs
 
-env LD_PRELOAD=../gperftools/.libs/libprofiler.so \
-    DYLD_INSERT_LIBRARIES=../gperftools/.libs/libprofiler.dylib \
-    CPUPROFILE=report.prof ./build/testharness $@
+env LD_PRELOAD=$GPERFTOOLS_ROOT/.libs/libprofiler.so \
+    DYLD_INSERT_LIBRARIES=$GPERFTOOLS_ROOT/.libs/libprofiler.dylib \
+    CPUPROFILE=$GPERFTOOLS_REPORT ./build/testharness $@
 
-../gperftools/src/pprof --text build/testharness report.prof
+$GPERFTOOLS_ROOT/src/pprof --text build/testharness $GPERFTOOLS_REPORT
