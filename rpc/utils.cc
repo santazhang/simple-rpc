90b38cb776201a45e4a10172eeeaa605870a9680
Santa Zhang <santa1987@gmail.com>
2013-10-27 17:49:15 -0400

    fix indent spacing

--- rpc/utils.cc
+++ rpc/utils.cc
@@ -28,56 +28,56 @@ int set_nonblocking(int fd, bool nonblocking) {
 }
 
 int find_open_port() {
-  int fd = socket(AF_INET, SOCK_STREAM, 0);
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
 
-  addrinfo *local_addr;
+    addrinfo *local_addr;
 
-  addrinfo hints;
-  bzero(&hints, sizeof(hints));
-  hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
+    addrinfo hints;
+    bzero(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-  if (getaddrinfo("localhost", NULL, NULL, &local_addr) != 0) {
-    Log_fatal("Failed to getaddrinfo");
-  }
-
-  int port = -1;
-
-  for (int i = 1024; i < 65000; ++i) {
-    ((sockaddr_in*)local_addr->ai_addr)->sin_port = i;
-    if (::bind(fd, local_addr->ai_addr, local_addr->ai_addrlen) != 0) {
-      continue;
+    if (getaddrinfo("localhost", NULL, NULL, &local_addr) != 0) {
+        Log_fatal("Failed to getaddrinfo");
     }
 
-    sockaddr_in addr;
-    socklen_t addrlen;
-    if (getsockname(fd, (sockaddr*)&addr, &addrlen) != 0) {
-      Log_fatal("Failed to get socket address");
+    int port = -1;
+
+    for (int i = 1024; i < 65000; ++i) {
+        ((sockaddr_in*)local_addr->ai_addr)->sin_port = i;
+        if (::bind(fd, local_addr->ai_addr, local_addr->ai_addrlen) != 0) {
+            continue;
+        }
+
+        sockaddr_in addr;
+        socklen_t addrlen;
+        if (getsockname(fd, (sockaddr*)&addr, &addrlen) != 0) {
+            Log_fatal("Failed to get socket address");
+        }
+
+        port = i;
+        break;
     }
 
-    port = i;
-    break;
-  }
+    freeaddrinfo(local_addr);
+    ::close(fd);
 
-  freeaddrinfo(local_addr);
-  ::close(fd);
+    if (port != -1) {
+        Log_info("Found open port: %d", port);
+        return port;
+    }
 
-  if (port != -1) {
-    Log_info("Found open port: %d", port);
-    return port;
-  }
-
-  Log_fatal("Failed to find open port.");
-  return -1;
+    Log_fatal("Failed to find open port.");
+    return -1;
 }
 
 std::string get_host_name() {
-  char buffer[1024];
-  if (gethostname(buffer, 1024) != 0) {
-    Log_fatal("Failed to get hostname.");
-  }
-  return std::string(buffer);
+    char buffer[1024];
+    if (gethostname(buffer, 1024) != 0) {
+        Log_fatal("Failed to get hostname.");
+    }
+    return std::string(buffer);
 }
 
 }
