1f0a421d486f4d997e609a858463a0df93b65a5d
Santa Zhang <santa1987@gmail.com>
2013-12-02 00:06:57 -0500

    gperftools.sh to tap any binary

--- gperftools.sh
+++ gperftools.sh
@@ -5,8 +5,12 @@ export GPERFTOOLS_REPORT=report.prof
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GPERFTOOLS_ROOT/.libs
 export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:$GPERFTOOLS_ROOT/.libs
 
+target=$1
+
+shift
+
 env LD_PRELOAD=$GPERFTOOLS_ROOT/.libs/libprofiler.so \
     DYLD_INSERT_LIBRARIES=$GPERFTOOLS_ROOT/.libs/libprofiler.dylib \
-    CPUPROFILE=$GPERFTOOLS_REPORT ./build/testharness $@
+    CPUPROFILE=$GPERFTOOLS_REPORT $target $@
 
-$GPERFTOOLS_ROOT/src/pprof --text build/testharness $GPERFTOOLS_REPORT
+$GPERFTOOLS_ROOT/src/pprof --text $target $GPERFTOOLS_REPORT
